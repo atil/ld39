b4377caad90e608b84870dee3c0b007dff91cb50
atil <hyperion91@gmail.com>
2017-07-30 23:33:13 +0200

    Battery visual;

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    public Renderer Renderer;
+
+	void Update ()
+	{
+	    var s = Mathf.Abs(Mathf.Sin(Time.time * 8f));
+        var c = new Color(s,s,s);
+
+	    Renderer.sharedMaterial.SetColor("_EmissionColor", c);
+
+    }
+}
